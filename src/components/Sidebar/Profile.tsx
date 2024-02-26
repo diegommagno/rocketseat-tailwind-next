e77ef75a8f4ccb217e00c112f4ca75b0d571a8c5
Diego Magno <contact@diegommagno.com>
2024-02-26 23:55:19 +0000

    :sparkles: Feat: Add Profile component to Sidebar

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,20 @@
+import { LogOut } from 'lucide-react'
+
+export function Profile() {
+  return (
+    <div className="grid-cols-profile grid items-center gap-3">
+      <img
+        src="https://github.com/diegommagno.png"
+        className="h-10 w-10 rounded-full"
+        alt=""
+      />
+      <div className="flex flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">Diego Magno</span>
+        <span className="text-sm text-zinc-500">diego@dmm.studio</span>
+      </div>
+      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  )
+}
