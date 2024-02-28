b999accf76be7e086f49efe3bf9fb38451332172
Diego Magno <contact@diegommagno.com>
2024-02-28 23:56:40 +0000

    :sparkles: Feat: Remove search functionality from Sidebar component and add input.tsx

---
+++ src/components/input.tsx
@@ -0,0 +1,15 @@
+import { Search } from 'lucide-react'
+
+export interface InputProps {}
+
+export function Input(props: InputProps) {
+  return (
+    <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+      <Search className="h-5 w-5 text-zinc-500" />
+      <input
+        className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+        placeholder="Search"
+      />
+    </div>
+  )
+}
