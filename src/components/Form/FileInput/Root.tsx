4760534a6cb1655f4190d32b46f05b5bd000a6e2
Diego Magno <contact@diegommagno.com>
2024-03-05 23:58:52 +0000

    :sparkles: Feat: Add Context and Image preview

--- src/components/Form/FileInput/Root.tsx
+++ src/components/Form/FileInput/Root.tsx
@@ -1,7 +1,17 @@
-import { ComponentProps } from 'react'
+import { ComponentProps, createContext } from 'react'
 
 export type RootProps = ComponentProps<'div'>
 
+type FileInputContextType = {
+  id: string
+}
+
+const FileInputContext = createContext({})
+
 export function Root(props: RootProps) {
-  return <div {...props} />
+  return (
+    <FileInputContext.Provider value=({ id: })>
+      <div {...props} />
+    </FileInputContext.Provider>
+  )
 }
