827a9f246941dd6b4346c2dcf3d7b8a6fd965b94
diegommagno <contact@diegommagno.com>
2024-03-06 23:21:17 +0000

    :sparkles: Feat: Handle file selection and display image preview

--- src/components/Form/FileInput/ImagePreview.tsx
+++ src/components/Form/FileInput/ImagePreview.tsx
@@ -1,9 +1,34 @@
+'use client'
+
 import { User } from 'lucide-react'
+import { useFileInput } from './Root'
+import { useMemo } from 'react'
 
 export function ImagePreview() {
-  return (
-    <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
-      <User className="h-8 w-8 text-violet-500" />
-    </div>
-  )
+  const { files } = useFileInput()
+
+  const previewURL = useMemo(() => {
+    if (files.length === 0) {
+      return null
+    }
+
+    return URL.createObjectURL(files[0])
+  }, [files]) // Forma de memorizar um cálculo que é custoso para o navegador e somente calcular essa função/variável apenas quando algo mudar. Se não, caso o componente seja renderizado por outro motivo, ele não vai recalcular essa informação.
+
+  if (previewURL === null) {
+    return (
+      <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
+        <User className="h-8 w-8 text-violet-500" />
+      </div>
+    )
+  } else {
+    return (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img
+        src={previewURL}
+        alt=""
+        className="h-16 w-16 rounded-full object-cover"
+      />
+    )
+  }
 }
--- src/components/Form/FileInput/Root.tsx
+++ src/components/Form/FileInput/Root.tsx
@@ -1,20 +1,29 @@
 'use client'
 
-import { ComponentProps, createContext, useContext, useId } from 'react'
+import {
+  ComponentProps,
+  createContext,
+  useContext,
+  useId,
+  useState,
+} from 'react'
 
 export type RootProps = ComponentProps<'div'>
 
 type FileInputContextType = {
   id: string
+  files: File[]
+  onFilesSelected: (file: File[]) => void
 }
 
 const FileInputContext = createContext({} as FileInputContextType)
 
 export function Root(props: RootProps) {
   const id = useId() // Cria um id unico e persiste esse id entre as renderizacoes de componentes
+  const [files, setFiles] = useState<File[]>([])
 
   return (
-    <FileInputContext.Provider value={{ id }}>
+    <FileInputContext.Provider value={{ id, files, onFilesSelected: setFiles }}>
       <div {...props} />
     </FileInputContext.Provider>
   )
